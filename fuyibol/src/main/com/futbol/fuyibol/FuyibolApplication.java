337d0e6f3dc436d34a74155500a066e1ebd53c55
David <davidpiraban003@gmail.com>
2026-06-09 07:01:28 -0500

    subiendo primer proyecto

---
+++ fuyibol/src/main/com/futbol/fuyibol/FuyibolApplication.java
@@ -0,0 +1,13 @@
+package com.futbol.fuyibol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FuyibolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FuyibolApplication.class, args);
+	}
+
+}
---
+++ fuyibol/src/test/java/com/futbol/fuyibol/FuyibolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.futbol.fuyibol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FuyibolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
